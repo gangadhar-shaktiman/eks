dda6982aa00a8b2d6bb214d17610e5bc4b452ae7
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2023-08-27 02:29:54 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -4,16 +4,10 @@ data "aws_subnet" "existing_subnets" {
 }
 
 resource "aws_subnet" "example_subnets" {
-  count  = length(var.subnet_ids)
+  count  = length(data.aws_subnet.existing_subnets)
   vpc_id = "vpc-011f1b733d94aa911"
-  
-  dynamic "source" {
-    for_each = data.aws_subnet.existing_subnets[count.index].id
-    content {
-      subnet_id = source.value
-    }
-  }
-  
+  cidr_block = data.aws_subnet.existing_subnets[count.index].cidr_block
+
   tags = {
     "Name"                       = "public-ap-south-1"
     "kubernetes.io/role/elb"     = "1"
