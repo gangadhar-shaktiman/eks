a915e81fd8e6bbea0e2bcc8e6a31c329d3f19c72
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2023-08-27 01:59:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-offsetmax"
-    key            = var.key
+    key            = "eks-test-Graphql-db-dev/terraform.tfstate"
     region         = "ap-south-1"
       
   }
